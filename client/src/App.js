7fa7a784a4cc5d7db2c897f007f8d05aaa36bd41
steven rosa <scrosa13@outlook.com>
2022-10-04 12:39:52 -0400

    updating client docs

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+
 
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttplink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Navbar from './components/Navbar';
 
 const httpLink = createHttplink({
   uri: '/graphql'
