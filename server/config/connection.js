166ee87fa11a834fd757912cf79a5a9e3c528c88
steven rosa <scrosa13@outlook.com>
2022-10-05 18:22:06 -0400

    heroku updates

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://srosa:googlebooks@cluster011.jd6uus3.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URI || '', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
